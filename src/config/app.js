a7a830b6a5301df6988fae113c24cec653ad9b81
tony-tripulca <tony.tripulca@gmail.com>
2025-03-14 02:05:10 +0800

    App urls

--- src/config/app.js
+++ src/config/app.js
@@ -5,13 +5,15 @@ const env = {
     ticketService: "http://revive-ticket-service:8803",
     branchService: "http://revive-branch-service:8804",
     loggerService: "http://revive-logger-service:8805",
+    resourceService: "http://revive-resource-service:8806",
   },
   uat: {
-    databaseService: "http://revive-database-service:8801",
-    userService: "http://revive-user-service:8802",
-    ticketService: "http://revive-ticket-service:8803",
-    branchService: "http://revive-branch-service:8804",
-    loggerService: "http://revive-logger-service:8805",
+    databaseService: "http://nadescrib.com:8801",
+    userService: "http://nadescrib.com:8802",
+    ticketService: "http://nadescrib.com:8803",
+    branchService: "http://nadescrib.com:8804",
+    loggerService: "http://nadescrib.com:8805",
+    resourceService: "http://nadescrib.com:8806",
   },
 };
 
