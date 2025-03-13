6f759059119f3c1ba500b57f37331a4404a4ba95
tony-tripulca <tony.tripulca@gmail.com>
2025-03-14 03:22:29 +0800

    Ignored watch on uploads

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       namespace: "revive-resource-service",
       script: "./src/index.js",
       watch: ["./src", "./src/*.js"],
+      ignore_watch: ["src/uploads"],
       output: "./logs/out.log",
       error: "./logs/error.log",
     },
